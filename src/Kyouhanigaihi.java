79b7c382a03b8d69a94e22ae71a6a2ad9395ae43
MatsudaKazuhito <12.27.worldbeansveloce@i.softbank.jp>
2015-11-17 17:22:37 +0900

    tekitoudayo

---
+++ src/Kyouhanigaihi.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class Kyouhanigaihi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Kyouhanigaihi");
+		for(int j=1;j<=9;j++){
+            for(int i=1;i<=9;i++){
+                  System.out.print(i*j+"\f");
+            	}
+         System.out.println();
+
+		}
+
+	}
+	}
